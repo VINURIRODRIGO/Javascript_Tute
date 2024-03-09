520c754c4007099b41534d3e8645445067b954ac
Harini Rodrigo <77738765+HARINIRODRIGO@users.noreply.github.com>
2024-03-09 12:31:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ console.log(name.toUpperCase());
 
 var twit =  prompt("Add a message");
 alert(twit.slice(0,129); // 128 characters 
+
+var name = prompt("Hello 👋\n What is your name?")
+var cappitalizeFirstLetter = name.slice(0,1).toUpperCase();
+var restOfName = name.slice(1,name.length);
+alert(cappitalizeFirstLetter+restOfName);

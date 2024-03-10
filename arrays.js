69dd457078b00222a214b63bc2af1c0b8c248881
Vinuri Rodrigo <77739211+VINURIRODRIGO@users.noreply.github.com>
2024-03-10 11:59:06 +0530

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -8,4 +8,28 @@ for (i=0; i<10; i++)
 {
 numbersFunc();
 }
-console.log(numbers);  
+console.log(numbers);
+
+// Fizz bus game
+var fizzBuzzGame=[];
+
+function FizzBuzz(number){
+    if(number%5===0 && number%3===0){
+        console.log(number+": Buzz");
+        fizzBuzzGame.push("FizzBuzz")
+    }
+    else if(number%5===0){
+        fizzBuzzGame.push("Buzz")
+    }
+    else if(number%3===0){
+        fizzBuzzGame.push("Fizz")
+    }
+    else{
+        fizzBuzzGame.push(number)
+    }
+}
+for(i=1; i<101; i++){
+    FizzBuzz(i);
+}
+
+console.log(fizzBuzzGame);

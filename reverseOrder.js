b16648b70da711bdccb2f41f5dfc1cc6b65daea1
Vinuri Rodrigo <77739211+VINURIRODRIGO@users.noreply.github.com>
2024-03-11 03:40:20 +0530

    Update reverseOrder.js

--- reverseOrder.js
+++ reverseOrder.js
@@ -1,6 +1,5 @@
 // Reverse order a list
 function getReversed(source){
-    var result = [];
     for(var i=0; i<source.length; i++){
         for(var j=i+1; j<source.length; j++){
             if(source[i]<source[j]){
